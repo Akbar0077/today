b80ed7e4b0af61fbf6b8442b3aaf3e30e3f75328
Akbar <akbarece007gmail.com>
2023-11-17 12:54:23 +0530

    today program

---
+++ b.py
@@ -0,0 +1,102 @@
+'''
+a=int(input("Enter the value:"))
+
+if (a % 400==0) and (a % 100==0):
+    print("It is a leap year")
+elif (a % 4==0) and (a % 100 != 0):
+    print("It is a leap year")
+    
+else:
+    print("It is not  a leap year")
+ 
+a=int(input("Enter the value:"))
+'''
+'''
+a=int(input("Enter the value:"))
+b=int(input("Enter the value:"))
+c=int(input("Enter the value:"))
+
+if a>=b and a>=c:
+    print("a")
+elif b>=a and b>=c:
+    print("b")
+else:
+    print(Enter tht("c")
+    
+a=input("Enter the input : ")
+b=int(a) + 10
+print(type(b))
+print(b)
+
+a=int(input("Enter the value:"))
+b=int(input("Enter the value:"))
+c=int(input("Enter the value:"))
+if (a>b):
+    print("A is greater than b")
+  elif (a==b):
+    print("A is equal to B")
+  if (b>c):
+    print("B is greater than C")
+   elif (b==c):
+    print("B is equal to C")
+     if (c>a):
+    print("C is greater than A")
+     elif (c==a):
+    print("C is equal to A")
+
+    
+    
+a=int(input("Enter the value:"))
+b=int(input("Enter the value:"))
+c=int(input("Enter the value:"))
+
+
+if (a==b):
+    if (a==c):
+        print(" all values are equal")
+    elif (b==c):
+        print("b is equal to c")
+    else:
+        print("A is equal to b")
+elif (b==c):
+    print("B is equal to c")
+elif (a==c):
+    print("A is equal to C")
+elif (a> b):
+    if (a>c):
+        print("A is greater")
+    else :
+        print("C is greater")
+
+
+elif (b>c): 
+    print("B is greater")        
+else:
+    print("c is greater")
+
+
+
+a=int(input("Enter the value:"))
+b=int(input("Enter the value:"))
+c=int(input("Enter the value:"))
+
+if (a>b):
+        print("A is greater")
+  if ( b>c):
+         print("B is greater")
+    if (c>a):
+        print ("C is graeter")
+a=input("Enter the value:")
+if (a=='a' or a=='e' or a=='i' or a=='o' or a=='u' ):
+    print("it is Vowels")
+else:
+    print("it is Consonants")
+a=[1,2,3,4,5]
+for i in a:
+    print (i)
+for i in range(-10,1,1):
+    print(i)'
+for i in range(1,11):
+    print(i,"Arun")
+    
+
